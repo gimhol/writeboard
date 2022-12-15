45d8d8f4cdc8d4f89ebd5e9e49469cb1d2b6109f
Gim <gimholg1992@foxmail.com>
2022-12-15 11:47:07 +0800

    fix: 修改椭圆的绘制

--- src/shape/oval/Shape.ts
+++ src/shape/oval/Shape.ts
@@ -5,24 +5,16 @@ import { ShapeNeedPath } from "../base/ShapeNeedPath"
 
 export class ShapeOval extends ShapeNeedPath<OvalData> {
   path(ctx: CanvasRenderingContext2D) {
-    const d = this.data
     const { x, y, w, h } = this.drawingRect()
-    const drawOffset = (d.w % 2) ? 0.5 : 0
-    // 贝塞尔曲线拟合椭圆
-    const kappa = 0.5522848
-    const ox = (w / 2) * kappa
-    const oy = (h / 2) * kappa
-    const xe = x + w
-    const ye = y + h
-    const xm = Math.floor(d.x + d.w / 2) + drawOffset
-    const ym = Math.floor(d.y + d.h / 2) + drawOffset
-    ctx.beginPath()
-    ctx.moveTo(x, ym)
-    ctx.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y)
-    ctx.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym)
-    ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye)
-    ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym)
-    ctx.closePath()
+    const r = (w > h) ? w : h;
+    const scale = { x: w / r, y: h / r };
+    ctx.save();
+    ctx.scale(scale.x, scale.y);
+    ctx.beginPath();
+    ctx.moveTo((x + w) / scale.x, y / scale.y);
+    ctx.arc(x / scale.x, y / scale.y, r, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.restore();
   }
 }
 
