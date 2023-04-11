2940754181c7de5663fdf91ae991f2e94af533aa
gim_z66 <gimholg1992@foxmail.com>
2023-04-11 12:17:37 +0800

    .

--- src/event/Observer.ts
+++ src/event/Observer.ts
@@ -1,40 +1,57 @@
-import { Callback, Emitter } from "./Emitter";
-export interface Listener {
-  times: number
-  target: EventTarget
-  type: string
-  callback: Callback | null
-  options?: boolean | AddEventListenerOptions
-  canceller: () => void
-}
+import { ICallback as ICallback, Emitter, ICancelFunc } from "./Emitter";
+export interface IListenerBase {
+  times: number;
+  type: string;
+  options?: boolean;
+  canceller: ICancelFunc;
+};
+export interface IEmitterListener extends IListenerBase {
+  target: Emitter;
+  callback: ICallback;
+};
+
+export interface INativeCallback { (e: Event): void; };
+export interface INativeListener extends IListenerBase {
+  target: EventTarget;
+  callback: INativeCallback;
+};
+
+export type Listener = IEmitterListener | INativeListener
+
 export interface IObserver {
-  listenTo(
-    target: EventTarget,
-    type: string,
-    callback: Callback | null,
-    options?: boolean | AddEventListenerOptions
-  ): () => void
+  listenTo(target: EventTarget, type: string, callback: INativeCallback): ICancelFunc;
+  listenTo(target: Emitter, type: string, callback: ICallback): ICancelFunc;
   destory(): void
-}
+};
+
 export class Observer implements IObserver {
   private _listeners: Listener[] = [];
-  listenTo(
-    target: EventTarget,
-    type: string,
-    callback: Callback | null,
-    options?: boolean | AddEventListenerOptions
-  ) {
+
+  listenTo<K extends keyof GlobalEventHandlersEventMap>(target: EventTarget, type: K, callback: (e: GlobalEventHandlersEventMap[K]) => any): ICancelFunc;
+  listenTo(target: EventTarget, type: string, callback: INativeCallback): ICancelFunc;
+  listenTo(target: Emitter, type: string, callback: ICallback): ICancelFunc;
+  listenTo(target: Emitter | EventTarget, type: string, callback: ICallback | INativeCallback): ICancelFunc;
+  listenTo(target: Emitter | EventTarget, type: string, callback: ICallback | INativeCallback) {
     if (target instanceof Emitter) {
-      const { canceller } = target.addEventListener(type, callback, options);
-      return canceller
+      const listener = target.addEventListener(type, callback as ICallback);
+      this._listeners.push(listener);
+      return listener.canceller;
+    } else {
+      const canceller = () => target.removeEventListener(type, callback as INativeCallback);
+      target.addEventListener(type, callback as INativeCallback, undefined);
+      const listener: INativeListener = {
+        times: -1,
+        target,
+        type,
+        callback: callback as INativeCallback,
+        canceller
+      }
+      this._listeners.push(listener);
+      return canceller;
     }
-    const canceller = () => target.removeEventListener(type, callback, options)
-    const listener = { times: -1, target, type, callback, canceller }
-    target.addEventListener(type, callback, options);
-    this._listeners.push(listener);
-    return canceller
   }
-  destory() {
+
+  destory(): void {
     this._listeners.forEach(v => v.canceller());
   }
 }
